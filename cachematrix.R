7a4e903c68e7f4bb813f4e3b4c1358f92114ed25
JoseEvandro1989 <j.evandro1989@gmail.com>
2017-10-03 11:37:56 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
-
+## makeCacheMatrix is a function which creates a special "matrix" object that can 
+## cache its inverse for the input (which is an invertible square matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
